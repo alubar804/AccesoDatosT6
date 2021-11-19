3ae5b2aec91e6ea206d2dd2a337ea9e1ff35ddb5
alubar804 <alubar804@ieselcaminas.org>
2021-11-19 20:02:28 +0100

    .

---
+++ src/ejemplos/Exemple3_EsborratDefectuos.kt
@@ -0,0 +1,24 @@
+package ejemplos
+
+import com.db4o.Db4oEmbedded
+import com.db4o.ObjectContainer
+import com.db4o.ObjectSet
+
+import classesEmpleat.Empleat
+
+fun main() {
+
+    val bd = Db4oEmbedded.openFile ("Empleats.db4o")
+    val patro = Empleat("22222222b")
+
+    // Si posàrem ací db.delete(patro) no tindría efecte, perquè e no es
+    // correspon amb cap instància de la BD
+
+    val llista = bd.queryByExample<Empleat>(patro)
+    if (llista.hasNext()) {
+        val e = llista.next()
+        bd.delete(e)
+    }
+    bd.close()
+}
+
---
+++ src/ejemplos/Exemple4_EsborratCorrecte.kt
@@ -0,0 +1,27 @@
+package ejemplos
+
+
+import com.db4o.Db4oEmbedded
+import com.db4o.ObjectContainer
+import com.db4o.ObjectSet
+
+import classesEmpleat.Empleat
+
+fun main() {
+    val conf = Db4oEmbedded.newConfiguration()
+    conf.common().objectClass("classesEmpleat.Empleat").cascadeOnDelete(true)
+
+    val bd = Db4oEmbedded.openFile(conf,"Empleats.db4o")
+    val patro = Empleat("33333333c")
+
+    // Si posàrem ací db.delete(patro) no tindría efecte, perquè e no es
+    // correspon amb cap instància de la BD
+
+    val llista = bd.queryByExample<Empleat>(patro)
+    if (llista.hasNext()) {
+        val e = llista.next()
+        bd.delete(e)
+    }
+    bd.close()
+}
+
